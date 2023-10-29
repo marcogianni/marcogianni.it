e8ce171dc8c643334da97050be760469123bc9b3
Marco Gianni <gian93ma50@gmail.com>
2023-10-29 17:46:50 +0100

    test

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,45 @@
+import matter from "gray-matter";
+import { readFileSync, readdirSync } from "fs";
+import path from "path";
+import Link from "next/link";
+
+const blogDir = "blog-posts";
+
 export default function BlogPosts() {
-  return <div>Page Blog</div>;
+  const files = readdirSync(path.join(blogDir));
+
+  const blogPosts = files.map((filename) => {
+    const fileContent = readFileSync(path.join(blogDir, filename), "utf-8");
+    const { data: frontMatter } = matter(fileContent);
+    return {
+      meta: frontMatter,
+      slug: filename.replace(".mdx", ""),
+    };
+  });
+
+  return (
+    <main className="lg:container lg:mx-auto pl-6 pr-6 pt-6 relative z-10">
+      <h1 className="text-3xl font-bold">My Blogging Site</h1>
+
+      <section className="py-10">
+        <h2 className="text-2xl font-bold">Latest Blogs</h2>
+
+        <div className="py-2">
+          {blogPosts.map((blog) => (
+            <Link href={"/blogs/" + blog.slug} passHref key={blog.slug}>
+              <div className="py-2 flex justify-between align-middle gap-2">
+                <div>
+                  <h3 className="text-lg font-bold">{blog.meta.title}</h3>
+                  <p className="text-gray-400">{blog.meta.description}</p>
+                </div>
+                <div className="my-auto text-gray-400">
+                  <p>{blog.meta.date}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,17 @@ export default function Navbar() {
       <div className="flex h-14 items-center p-6">
         <div className="md:flex">
           <Link href="/" className="mr-4 flex items-center">
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-3">
               <div className="pl-2 font-extrabold tracking-wider">
                 MARCOGIANNI.IT
+              </div>
+              <div className="ml-6">
+                <Link
+                  href="/blog"
+                  className="transition-colors hover:text-foreground/80 text-foreground/60 font-medium text-md"
+                >
+                  Blog
+                </Link>
               </div>
             </div>
           </Link>
