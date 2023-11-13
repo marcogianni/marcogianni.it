66ece65096a0d61fa8b7d3fc37693a2a7f1ae0f0
Marco Gianni <gian93ma50@gmail.com>
2023-11-13 09:31:39 +0100

    cc

--- components/email/TemplateUser.tsx
+++ components/email/TemplateUser.tsx
@@ -4,10 +4,10 @@ import { EmailFormValues } from "./ContactForm";
 const TemplateUser: React.FC<Readonly<EmailFormValues>> = ({ name }) => (
   <div>
     <h1>Thank you, {name}!</h1>
-    <p>
+    <h5>
       This email is simply a confirmation of receipt of your contact. I will get
       back to you as soon as possible.
-    </p>
+    </h5>
   </div>
 );
 
