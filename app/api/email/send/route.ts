75f172393f4cbfdbd9d51ba6e1bed64beca7bc46
Marco Gianni <gian93ma50@gmail.com>
2023-11-13 09:30:38 +0100

    cc

--- app/api/email/send/route.ts
+++ app/api/email/send/route.ts
@@ -8,18 +8,18 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req: NextRequest) {
   const body = await req.json();
   try {
-    const { data, error } = await resend.emails.send({
-      from: "info@marcogianni.it",
-      to: [body.email],
-      subject: body.subject,
-      react: TemplateUser(body) as React.ReactElement,
-    });
-
     const { data: ownerData, error: errorData } = await resend.emails.send({
       from: "info@marcogianni.it",
       to: ["info@marcogianni.it"],
       subject: body.subject,
       react: TemplateOwner(body) as React.ReactElement,
+    });
+
+    const { data, error } = await resend.emails.send({
+      from: "info@marcogianni.it",
+      to: [body.email],
+      subject: body.subject,
+      react: TemplateUser(body) as React.ReactElement,
     });
 
     if (error || errorData) {
