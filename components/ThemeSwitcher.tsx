e02e7aa5b2434ed4a6fc3220523f71b7d4cd1acd
Marco Gianni <gian93ma50@gmail.com>
2023-11-15 13:36:00 +0100

    fixes

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -29,9 +29,6 @@ export default function ThemeSwitcher() {
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
